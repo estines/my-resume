12e1c1fe371472fceb9c552d507c4cf48dab6d4d
estines <estines.nop@gmail.com>
2018-02-28 23:44:32 +0700

    init project

---
+++ app.js
@@ -0,0 +1,19 @@
+const koa = require('koa')
+const fs = require('fs')
+const path = require('path')
+const static = require('koa-static')
+const bodyparser = require('koa-bodyparser')
+const Router = require('koa-router')
+
+const app = new koa()
+const router = new Router()
+    .get('*', function* () {
+        this.body = fs.readFileSync(path.resolve(path.join('build', 'index.html')), 'utf-8')
+    })
+
+app.use(static(path.resolve(__dirname, 'build')))
+app.use(bodyparser())
+app.use(router.routes())
+app.listen(3000, () => {
+    console.log('This application is running on port 3000')
+})
