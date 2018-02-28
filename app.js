e3aec693b39ebf6a77a8368f1f8f1df9b950b9fe
estines <estines.nop@gmail.com>
2018-03-01 00:43:37 +0700

    set port run on environment

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const path = require('path')
 const static = require('koa-static')
 const bodyparser = require('koa-bodyparser')
 const Router = require('koa-router')
+const port = process.env.PORT || 3000
 
 const app = new koa()
 const router = new Router()
     .get('/api', async ctx => {
-        ctx.body = `Hello API`
+        ctx.body = `Hello API555`
     })
     //! set route before generator function!!
     .get('*', function* () {
@@ -18,6 +19,6 @@ const router = new Router()
 app.use(static(path.resolve(__dirname, 'build')))
 app.use(bodyparser())
 app.use(router.routes())
-app.listen(3000, () => {
-    console.log('This application is running on port 3000')
+app.listen(port, () => {
+    console.log(`This application is running on port ${port}`)
 })
